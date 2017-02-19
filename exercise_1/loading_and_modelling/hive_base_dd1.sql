e19c85f81dc178f56b6e410b934bab6a2130bb0e
w205 <w205@ip-172-31-14-11.ec2.internal>
2017-02-19 08:05:12 +0000

    new

--- exercise_1/loading_and_modelling/hive_base_dd1.sql
+++ exercise_1/loading_and_modelling/hive_base_dd1.sql
@@ -12,14 +12,33 @@ CREATE EXTERNAL TABLE hospitals
   phone_number string,
   hospital_type string,
   hospital_ownership string,
-  emergency_service string
+  emergency_service string,
+  meet_EHRS string,
+  hospital_overall_rating string,
+  hospital_overall_rating_footnote string,
+  mortality_national_comparison string,
+  mortality_national_comparison_footnote string,
+  safety_of_care_comparison string,
+  safety_of_care_comparison_footnote string,
+  readmission_national_comparison string,
+  readmission_national_comparison_footnote string,
+  patient_experience_comparison string,
+  patient_experiene_comparison_footnote string,
+  effectiveness_of_care_comparison string,
+  effectiveness_of_care_comparison_footnote string,
+  timeliness_of_care_comparison string,
+  timeliness_of_care_comparison_footnote string,
+  efficient_use_of_medical_imaging_comparison string,
+  efficient_use_of_medical_imaging_comparison_footnote string 
 )
-ROW FORMAT SERDE'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+
+ROW FORMAT SERDE
+'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES(
   "separatorChar"=",",
   "quoteChar"='"',
   "escapeChar"="\\"
 )
 STORED AS TEXTFILE
-LOCATION "/user/w205/hospital_compare"
+LOCATION "/user/w205/hospital_compare/hospitals"
 ;
