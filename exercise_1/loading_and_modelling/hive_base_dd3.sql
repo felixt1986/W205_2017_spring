f91448fc50b0e1501ca213cef7077a88680841fe
w205 <w205@ip-172-31-14-11.ec2.internal>
2017-02-19 08:52:20 +0000

    new

---
+++ exercise_1/loading_and_modelling/hive_base_dd3.sql
@@ -0,0 +1,35 @@
+DROP TABLE hospitals;
+
+CREATE EXTERNAL TABLE hospitals
+(
+  provider_id string,
+  hospital_name string,
+  address string,
+  city string,
+  state string,
+  zip_code string,
+  county_name string,
+  phone_number string,
+  measure_name string,
+  measure_id string,
+  compared_to_national string,
+  denominator string,
+  score string,
+  lower_est string,
+  higher_est string,
+  footnote string,
+  measure_SD string,
+  measure_ED string 
+)
+
+ROW FORMAT SERDE
+'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES(
+  "separatorChar"=",",
+  "quoteChar"='"',
+  "escapeChar"="\\"
+)
+STORED AS TEXTFILE
+LOCATION "/user/w205/hospital_compare/readmissions"
+;
+
---
+++ exercise_1/loading_and_modelling/hive_base_dd4.sql
@@ -0,0 +1,23 @@
+DROP TABLE hospitals;
+
+CREATE EXTERNAL TABLE hospitals
+(
+  measure_name string,
+  measure_id string,
+  measure_SQ string,
+  measure_SD string,
+  measure_EQ string,
+  measure_ED string 
+)
+
+ROW FORMAT SERDE
+'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES(
+  "separatorChar"=",",
+  "quoteChar"='"',
+  "escapeChar"="\\"
+)
+STORED AS TEXTFILE
+LOCATION "/user/w205/hospital_compare/measures"
+;
+
